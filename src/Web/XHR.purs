6ac3092ae62f9252280c61e87de9c56204604b06
Gary Burgess <gary.burgess@gmail.com>
2018-03-17 16:59:40 +0000

    Add missing re-exports

--- src/Web/XHR.purs
+++ src/Web/XHR.purs
@@ -3,5 +3,5 @@ module Web.XHR
   , module Web.XHR.XMLHttpRequest
   ) where
 
-import Web.XHR.Types (ReadyState(..), ResponseType, XHR, XMLHttpRequest, XMLHttpRequestUpload, arrayBuffer, blob, document, string, xmlHttpRequestToEventTarget, xmlHttpRequestUploadToEventTarget)
-import Web.XHR.XMLHttpRequest (abort, getAllResponseHeaders, getResponseHeader, open, open', readyState, response, responseURL, send, sendBlob, sendDocument, sendString, setRequestHeader, setTimeout, setWithCredentials, status, statusText, timeout, upload, withCredentials, xmlHttpRequest)
+import Web.XHR.Types (FormData, ReadyState(..), ResponseType, XHR, XMLHttpRequest, XMLHttpRequestUpload, arrayBuffer, blob, document, string, xmlHttpRequestToEventTarget, xmlHttpRequestUploadToEventTarget)
+import Web.XHR.XMLHttpRequest (abort, getAllResponseHeaders, getResponseHeader, open, open', overrideMimeType, readyState, response, responseURL, send, sendArrayView, sendBlob, sendDocument, sendFormData, sendString, setRequestHeader, setTimeout, setWithCredentials, status, statusText, timeout, upload, withCredentials, xmlHttpRequest)
